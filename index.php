05340e415f69d0c03f29a0b680e6dfc37edcd536
tuankk1234567 <67895388+tuankk1234567@users.noreply.github.com>
2020-07-06 16:55:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <h1>My first PHP page</h1>
 
 <?php
-<img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="Flowers in Chania">
+
 echo "Hello World!";
 ?>
-
+<img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="Flowers in Chania">
 </body>
 </html>
